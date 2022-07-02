7b78d47abfb34458178b18dfec30cc60982087be
Daniel Grossman <93166334+daniel-grossman@users.noreply.github.com>
2022-07-01 17:37:58 -0700

    Add files via upload

---
+++ random password generator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+#using an OS-dependent cryptographic RNG source
+systemrandom = random.SystemRandom()
+
+#'all' is a string of all possible letters, numbers etc.
+all = string.ascii_letters + string.digits + string.punctuation
+
+#these lists store password lengths + passwords
+lengths = []
+passwords = []
+
+#user input for num. of passwords
+numPass = int(input('Enter number of passwords: '))
+
+#user input for length of each password
+for i in range(0, numPass):
+    print('Enter password no. {} length: '.format(i+1))
+    lengt = int(input())
+    lengths.append(lengt)
+
+#password creation using each length
+for i in lengths:
+    password = ''.join(systemrandom.sample(all, i))
+    passwords.append(password)
+
+for i in passwords:
+    print(i)
